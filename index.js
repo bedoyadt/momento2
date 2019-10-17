affe43e536951f0afb061f1810ea47211d006508
raul <git@git.com>
2019-10-17 18:56:27 -0500

    momento

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.json({
+      'mensaje' : 'Bienvenido'
+    })
+});
+app.get('/database', function (req, res) {
+  res.json({
+      'database' : 'momento2'
+  })
+});
+
+
+
+
+mongoose.connect('mongodb://localhost:27017/momento2', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err, res) => {
+    if(err) throw err;
+    console.log("Conectado a la DB");
+});
+
+
+let port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Servidor ONLINE en el puerto ${ port }`);
+});
